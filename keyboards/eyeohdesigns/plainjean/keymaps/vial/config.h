be0c3a3e2f8c849ac04a98179f5f6c130aa1bf2d
joedinkle <eodkeyboards@gmail.com>
2022-09-27 21:08:45 -0700

    Adding firmware for Plain Jean

---
+++ keyboards/eyeohdesigns/plainjean/keymaps/vial/config.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x1B, 0x30, 0x13, 0x8D, 0x29, 0x4C, 0x3E, 0x48}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
+
+#define TAPPING_TERM 125
