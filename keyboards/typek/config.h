1262a078c4df7140fb0fe394b060be4685bfad15
ze-or <32355899+ze-or@users.noreply.github.com>
2025-08-04 22:42:40 -0700

    type k keymap.c updated

--- keyboards/typek/config.h
+++ keyboards/typek/config.h
@@ -25,9 +25,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define WS2812_DMA_CHANNEL 6
 
 /* LED MAP
-		0 , 1 , 2														Three indicators
-		3 , 4 , 5														Three RGBs on left side
-		66, 67, 68														Three RGBs on right side
-		35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10,  9,  8,  7,  6	Left RGB wing (30 LEDs)
-		36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65	Right RGB wing (30 LEDs)
+		0, 1, 2	
+	Three indicators
+		3, 4, 5
+	Three RGBs on left side
+		6, 7, 8
+	Three RGBs on right side
+		38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9
+	Left RGB wing (30 LEDs)
+		39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68
+	Right RGB wing (30 LEDs)
 		*/
--- keyboards/typek/keymaps/vial/keymap.c
+++ keyboards/typek/keymaps/vial/keymap.c
@@ -19,97 +19,99 @@ enum custom_keycode {
     LM_LCTL = QK_KB_0,
     LM_LGUI,
     LM_LALT,
-    US_REP,
 };
 
+#define MOD_LAYER 4
+static bool lctl_held = false;
+static bool lgui_held = false;
+static bool lalt_held = false;
+void handle_mod_layer(bool *held_flag, bool pressed, uint16_t kc_mod) {
+    *held_flag = pressed;
+    if (pressed) {
+        register_code(kc_mod);
+    } else {
+        unregister_code(kc_mod);
+    }
+    if (lctl_held || lgui_held || lalt_held) {
+        layer_on(MOD_LAYER);
+    } else {
+        layer_off(MOD_LAYER);
+    }
+}
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-	switch (keycode) {
-		case LM_LCTL:
-            if (record->event.pressed) {
-                layer_on(4);
-                register_code(KC_LCTL);
-            }else{
-                layer_off(4);
-                unregister_code(KC_LCTL);
-            }
+    switch (keycode) {
+        case LM_LCTL:
+            handle_mod_layer(&lctl_held, record->event.pressed, KC_LCTL);
             return false;
         case LM_LGUI:
-            if (record->event.pressed) {
-                layer_on(4);
-                register_code(KC_LGUI);
-            }else{
-                layer_off(4);
-                unregister_code(KC_LGUI);
-            }
+            handle_mod_layer(&lgui_held, record->event.pressed, KC_LGUI);
             return false;
         case LM_LALT:
-            if (record->event.pressed) {
-                layer_on(4);
-                register_code(KC_LALT);
-            }else{
-                layer_off(4);
-                unregister_code(KC_LALT);
-            }
-	}
+            handle_mod_layer(&lalt_held, record->event.pressed, KC_LALT);
+            return false;
+    }
     return true;
 }
 
+#define IME_TOG A(KC_GRV)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-[0] = LAYOUT_all( /* Base */
+[0] = LAYOUT_all( /* QWERTY */
     KC_ESC , KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,          KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , KC_BSLS,
     DF(0)  , KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,          KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC,
-    DF(1)  , KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,          KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_DEL,
-    DF(2)  , KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,          KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
-             KC_LGUI, KC_LALT,          KC_SPC , KC_BSPC, KC_6   ,          MO(6)  , QK_REP , KC_RSFT, KC_BSPC, KC_ENT , MO(7)  , A(KC_GRV)
+    DF(1)  , KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,          KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_DEL ,
+    DF(2)  , KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,          KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, MO(7)  ,
+             KC_LGUI, KC_LALT,          KC_SPC , KC_BSPC, KC_6   ,          MO(6)  , QK_REP , KC_RSFT, KC_BSPC, KC_ENT , IME_TOG, MO(4)
 ),
-[1] = LAYOUT_all(
+[1] = LAYOUT_all( /* Semimak */
     KC_ESC , KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,          KC_7   , KC_8   , KC_9   , KC_0   , KC_LBRC, KC_RBRC, KC_BSLS,
     DF(0)  , KC_TAB , KC_F   , KC_L   , KC_H   , KC_V   , KC_Z   ,          KC_Q   , KC_W   , KC_U   , KC_O   , KC_Y   , KC_SCLN, KC_EQL,
-    DF(1)  , LM_LCTL, KC_S   , KC_R   , KC_N   , KC_T   , KC_K   ,          KC_C   , KC_D   , KC_E   , KC_A   , KC_I   , KC_MINS, KC_DEL,
-    DF(2)  , KC_LSFT, KC_X   , KC_QUOT, KC_B   , KC_M   , KC_J   ,          XXXXXXX, KC_P   , KC_G   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
-             LM_LGUI, LM_LALT,          KC_SPC , KC_BSPC, KC_6   ,          MO(6)  , QK_REP , KC_RSFT, KC_BSPC, KC_ENT , MO(7)  , A(KC_GRV)
+    DF(1)  , LM_LCTL, KC_S   , KC_R   , KC_N   , KC_T   , KC_K   ,          KC_C   , KC_D   , KC_E   , KC_A   , KC_I   , KC_MINS, KC_DEL ,
+    DF(2)  , KC_LSFT, KC_X   , KC_QUOT, KC_B   , KC_M   , KC_J   ,          XXXXXXX, KC_P   , KC_G   , KC_COMM, KC_DOT , KC_SLSH, MO(7)  ,
+             LM_LGUI, LM_LALT,          KC_SPC , KC_BSPC, KC_6   ,          MO(6)  , QK_REP , KC_RSFT, KC_BSPC, KC_ENT , IME_TOG, MO(4)
 ),
-[2] = LAYOUT_all(
+[2] = LAYOUT_all( /* TypeHacK */
     KC_ESC , KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,          KC_7   , KC_8   , KC_9   , KC_0   , KC_LBRC, KC_RBRC, KC_BSLS,
-    DF(0)  , KC_TAB , KC_J   , KC_G   , KC_H   , KC_P   , KC_F   ,          KC_Q   , KC_V   , KC_O   , KC_U   , KC_SCLN, KC_SLSH, KC_EQL,
-    DF(1)  , LM_LCTL, KC_R   , KC_S   , KC_N   , KC_T   , KC_K   ,          KC_Y   , KC_I   , KC_A   , KC_E   , KC_L   , KC_MINS, KC_DEL,
-    DF(2)  , KC_LSFT, KC_Z   , KC_W   , KC_M   , KC_D   , KC_B   ,          XXXXXXX, KC_C   , KC_COMM, KC_QUOT, KC_DOT , KC_X   , KC_RSFT,
-             LM_LGUI, LM_LALT,          KC_SPC , KC_BSPC, KC_6   ,          MO(6)  , QK_REP , KC_RSFT, KC_BSPC, KC_ENT , MO(7)  , A(KC_GRV)
+    DF(0)  , KC_TAB , KC_J   , KC_G   , KC_H   , KC_P   , KC_F   ,          KC_Q   , KC_V   , KC_O   , KC_U   , KC_SCLN, KC_SLSH, KC_EQL ,
+    DF(1)  , LM_LCTL, KC_R   , KC_S   , KC_N   , KC_T   , KC_K   ,          KC_Y   , KC_I   , KC_A   , KC_E   , KC_L   , KC_MINS, KC_DEL ,
+    DF(2)  , KC_LSFT, KC_Z   , KC_W   , KC_M   , KC_D   , KC_B   ,          XXXXXXX, KC_C   , KC_COMM, KC_QUOT, KC_DOT , KC_X   , MO(7)  ,
+             LM_LGUI, LM_LALT,          KC_SPC , KC_BSPC, KC_6   ,          MO(6)  , QK_REP , KC_RSFT, KC_BSPC, KC_ENT , IME_TOG, MO(4)
 ),
-[3] = LAYOUT_all(
+[3] = LAYOUT_all( /* Colemak */
     KC_ESC , KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,          KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , KC_BSLS,
     DF(0)  , KC_TAB , KC_Q   , KC_W   , KC_F   , KC_P   , KC_G   ,          KC_J   , KC_L   , KC_U   , KC_Y   , KC_SCLN, KC_LBRC, KC_RBRC,
-    DF(1)  , LM_LCTL, KC_A   , KC_R   , KC_S   , KC_T   , KC_D   ,          KC_H   , KC_N   , KC_E   , KC_I   , KC_O   , KC_QUOT, KC_DEL,
-    DF(2)  , KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,          XXXXXXX, KC_K   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
-             LM_LGUI, LM_LALT,          KC_SPC , KC_BSPC, KC_6   ,          MO(6)  , QK_REP , KC_RSFT, KC_BSPC, KC_ENT , MO(7)  , A(KC_GRV)
+    DF(1)  , LM_LCTL, KC_A   , KC_R   , KC_S   , KC_T   , KC_D   ,          KC_H   , KC_N   , KC_E   , KC_I   , KC_O   , KC_QUOT, KC_DEL ,
+    DF(2)  , KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,          XXXXXXX, KC_K   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, MO(7)  ,
+             LM_LGUI, LM_LALT,          KC_SPC , KC_BSPC, KC_6   ,          MO(6)  , QK_REP , KC_RSFT, KC_BSPC, KC_ENT , IME_TOG, MO(4)
 ),
-[4] = LAYOUT_all(
+[4] = LAYOUT_all( /* QWERTY Mod Layer */
     _______, KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,          KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , KC_BSLS,
     TG(5)  , _______, KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,          KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC,
     _______, _______, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,          KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, _______,
     _______, _______, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,          KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, _______,
              _______, _______,          _______, _______, KC_6   ,          _______, _______, _______, _______, _______, _______, _______
 ),
-[5] = LAYOUT_all(
+[5] = LAYOUT_all( /* Mac Mods */
     _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______,
     _______, LM_LALT, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______,
              LM_LCTL, LM_LGUI,          _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______
 ),
-[6] = LAYOUT_all(
+[6] = LAYOUT_all( /* Number Symbol */
     _______, _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,          KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , _______,
     _______, KC_TILD, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC,          KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE, _______,
     _______, KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,          KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_BSLS, _______,
     _______, _______, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, KC_COMM,          _______, KC_DOT , KC_EQL , KC_PPLS, KC_MINS, KC_SLSH, _______,
              _______, _______,          _______, _______, KC_F6  ,          _______, _______, _______, _______, _______, MO(8)  , _______
 ),
-[7] = LAYOUT_all(
+[7] = LAYOUT_all( /* Navigation */
     _______, _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,          KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , _______,
-    DF(3)  , _______, KC_PGUP, KC_HOME, KC_UP  , KC_END , _______,          _______, _______, KC_WH_U, KC_BTN1, KC_MS_U, KC_BTN2, _______,
-    _______, _______, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, _______,          _______, _______, KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, _______,
-    _______, _______, KC_INS , KC_PSCR, KC_SCRL, KC_PAUS, _______,          _______, _______, _______, _______, _______, _______, _______,
+    DF(3)  , _______, KC_BTN2, KC_MS_U, KC_BTN1, KC_WH_U, _______,          _______, _______, KC_HOME, KC_UP  , KC_END , KC_PGUP, _______,
+    _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, _______,          _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______,
+    _______, _______, _______, _______, _______, _______, _______,          _______, _______, KC_INS , KC_PSCR, KC_SCRL, KC_PAUS, _______,
              _______, _______,          _______, _______, _______,          MO(8)  , _______, _______, _______, _______, _______, _______
 ),
 [8] = LAYOUT_all(
