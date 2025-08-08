ed04ed233ca1d5245382de012a059b498ebb9fbd
ze-or <32355899+ze-or@users.noreply.github.com>
2025-08-08 05:47:26 -0700

    Update keymap.c

--- keyboards/typek/keymaps/vial/keymap.c
+++ keyboards/typek/keymaps/vial/keymap.c
@@ -101,18 +101,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              LM_LCTL, LM_LGUI,          _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______
 ),
 [6] = LAYOUT_all( /* Number Symbol */
-    _______, _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,          KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , _______,
+    _______, _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,          KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , KC_F13 ,
     _______, KC_TILD, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC,          KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE, _______,
-    _______, KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,          KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_BSLS, _______,
+    _______, KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,          KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_BSLS, KC_F14 ,
     _______, _______, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, KC_COMM,          _______, KC_DOT , KC_EQL , KC_PPLS, KC_MINS, KC_SLSH, _______,
              _______, _______,          _______, _______, KC_F6  ,          _______, _______, _______, _______, _______, MO(8)  , _______
 ),
 [7] = LAYOUT_all( /* Navigation */
-    _______, _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,          KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , _______,
-    DF(3)  , _______, KC_BTN2, KC_MS_U, KC_BTN1, KC_WH_U, _______,          _______, _______, KC_HOME, KC_UP  , KC_END , KC_PGUP, _______,
-    _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, _______,          _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______,
+    _______, _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,          KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , KC_F13 ,
+    DF(3)  , KC_CAPS, KC_WH_U, KC_BTN2, KC_MS_U, KC_BTN1, _______,          _______, _______, KC_HOME, KC_UP  , KC_END , KC_PGUP, _______,
+    _______, _______, KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, _______,          _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_F14 ,
     _______, _______, _______, _______, _______, _______, _______,          _______, _______, KC_INS , KC_PSCR, KC_SCRL, KC_PAUS, _______,
-             _______, _______,          _______, _______, _______,          MO(8)  , _______, _______, _______, _______, _______, _______
+             _______, _______,          _______, _______, KC_F6  ,          MO(8)  , _______, KC_APP , _______, _______, _______, _______
 ),
 [8] = LAYOUT_all(
     QK_BOOT, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______,
